be7b4f2250685e78186c4ac5037622aa0c002db0
Tanya Ten <izvestiya9@mail.ru>
2024-09-23 22:13:59 +0300

    com1

---
+++ cmd/skill/main.go
@@ -0,0 +1,48 @@
+// пакеты исполняемых приложений должны называться main
+package main
+
+import (
+	"net/http"
+
+	"github.com/thalq/Alisa_Go/internal/logger"
+	"go.uber.org/zap"
+)
+
+// функция main вызывается автоматически при запуске приложения
+func main() {
+	// обрабатываем аргументы командной строки
+	parseFlags()
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	if err := logger.Initialize(flagLogLevel); err != nil {
+		return err
+	}
+
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+	// оборачиваем хендлер webhook в middleware с логированием
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte(`
+          {
+            "response": {
+              "text": "Извините, я пока ничего не умею"
+            },
+            "version": "1.0"
+          }
+        `))
+	logger.Log.Debug("sending HTTP 200 response")
+}
